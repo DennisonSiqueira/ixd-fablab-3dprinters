4da83c35dcce3932e04ff5fa0ac7162dc7e0620d
Roneilla <bumanlar@sheridancollege.ca>
2020-01-13 12:27:12 -0500

    Created test.html

---
+++ script.js
@@ -0,0 +1,14 @@
+// get current time
+var current_date = new Date();
+var current_time = current_date.getTime();
+var current_hours = current_date.getHours();
+var current_minutes = current_date.getMinutes();
+
+// display time
+var time_display = document.getElementById("current-time");
+
+time_display.innerHTML = "Current Time: <span style='font-weight: 700'> " + current_hours + ":" + current_minutes + "</span>";
+
+var printer1_availability = document.getElementById("availability-1");
+var printer2_availability = document.getElementById("availability-2");
+
